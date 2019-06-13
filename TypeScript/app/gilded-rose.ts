42c5233880ff10b08805243ceac080112a1d8b3c
Renna <renna@reemet.me>
2019-06-13 09:36:34 +0100

    Implement & refactor logic - Rewrite the updateQuality function - Fix math in tests - Add missing tests

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -14,56 +14,43 @@ export class GildedRose {
     items: Array<Item>;
 
     constructor(items = [] as Array<Item>) {
-        this.items = items;
+      this.items = items;
     }
 
     updateQuality() {
-        for (let i = 0; i < this.items.length; i++) {
-            if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'AgedBrie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if (this.items[i].quality > 0) {
-                    if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                        this.items[i].quality = this.items[i].quality - 1
-                    }
-                }
-            } else {
-                if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1
-                    if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].sellIn < 11) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                        if (this.items[i].sellIn < 6) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].sellIn = this.items[i].sellIn - 1;
-            }
-            if (this.items[i].sellIn < 0) {
-                if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'AgedBrie') {
-                    if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].quality > 0) {
-                            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                                this.items[i].quality = this.items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        this.items[i].quality = this.items[i].quality - this.items[i].quality
-                    }
-                } else {
-                    if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1
-                    }
-                }
-            }
-        }
+      this.items.forEach((item, idx) => {
+        // Converting the name to lower case because we don't trust user input
+        const name = item.name.toLowerCase();
+        let quality = item.quality;
+        const isAgedBrie = name === 'aged brie' || name === 'agedbrie';
+        // For the sake of simplicity in this test task, I'm only going to test if the name starts with the special
+        // case string. In real world, either the user input would be limited and an enum/mapping/etc created,
+        // or all the possible cases would be considered (ex. Metallica Backstage passes)
+        const isBackstagePass = name.startsWith('backstage pass');
+        const isSulfuras = name.startsWith('sulfuras');
+        const isConjured = name.startsWith('conjured');
 
-        return this.items;
+        if (isSulfuras) {
+          // Sulfuras is static - its quality is always 80, and it never expires, regardless of
+          // the original quality/expiry of the item
+          this.items[idx].quality = 80;
+          return;
+        } else if (isBackstagePass) {
+          // Reset quality to 0 if the sellIn date limit has passed
+          if (item.sellIn < 0) quality -= quality;
+          else quality += item.sellIn > 10 ? 1 : (item.sellIn > 5 ? 2 : 3);
+        }
+        else if (isAgedBrie) quality++;
+        else if (isConjured) quality -= 2;
+        else quality -= item.sellIn >= 0 ? 1 : 2;
+
+        if (quality < 0) quality = 0;
+        else if (quality > 50) quality = 50;
+
+        this.items[idx].quality = quality;
+        this.items[idx].sellIn--;
+      });
+
+      return this.items;
     }
 }
--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -4,32 +4,32 @@ import { Item, GildedRose } from '../app/gilded-rose';
 const items = [
   {
 		name: 'Regular',
-		item: new Item('foo', 9, 10),
-		expected: 4,
+		item: new Item('foo1', 9, 10),
+		expected: 5,
 		days: 5
 	},
   {
 		name: 'Last day',
-		item: new Item('foo', 9, 10),
-		expected: 2,
+		item: new Item('foo2', 9, 10),
+		expected: 1,
 		days: 9
 	},
   {
 		name: 'Low Quality (expired)',
-		item: new Item('foo', 3, 14),
-		expected: 1,
+		item: new Item('foo3', 3, 14),
+		expected: 0,
 		days: 9
 	},
  {
 		name: 'High Quality (expired)',
-		item: new Item('foo', 3, 94),
+		item: new Item('foo4', 3, 94),
 		expected: 39,
-		days: 30
+		days: 8
 	},
   {
 		name: 'Low Quality Aged Brie',
 		item: new Item('Aged Brie', 9, 10),
-    expected: 18,
+    expected: 19,
 		days: 9
 	},
   {
@@ -41,7 +41,7 @@ const items = [
   {
 		name: 'AgedBrie',
 		item: new Item('AgedBrie', 3, 3),
-		expected: 50,
+		expected: 33,
 		days: 30
 	},
   {
@@ -52,31 +52,31 @@ const items = [
 	},
   {
     name: 'Backstage pass more than 10 days away',
-    item: new Item('Backstage passes to a TAFKAL80ETC concert', 9, 14),
-    expected: 10,
+    item: new Item('Backstage passes to a TAFKAL80ETC concert 1', 9, 14),
+    expected: 16,
     days: 1
   },
   {
     name: 'Backstage pass less than 10 days away',
-    item: new Item('Backstage passes to a TAFKAL80ETC concert', 9, 14),
-    expected: 16,
+    item: new Item('Backstage passes to a TAFKAL80ETC concert 2', 9, 14),
+    expected: 25,
     days: 5
   },
   {
     name: 'Backstage pass less than 5 days away',
-    item: new Item('Backstage passes to a TAFKAL80ETC concert', 9, 14),
-    expected: 28,
+    item: new Item('Backstage passes to a TAFKAL80ETC concert 3', 9, 14),
+    expected: 40,
     days: 10
   },
   {
 		name: 'Backstage pass on last day',
-		item: new Item('Backstage passes to a TAFKAL80ETC concert', 9, 10),
-		expected: 30,
+		item: new Item('Backstage passes to a TAFKAL80ETC concert 4', 9, 10),
+		expected: 33,
 		days: 9
 	},
   {
 		name: 'Backstage pass after sell by',
-		item: new Item('Backstage passes to aTAFKAL80ETC concert', 9, 50),
+		item: new Item('Backstage passes to aTAFKAL80ETC concert 5', 9, 50),
 		expected: 0,
 		days: 19
 	},
@@ -100,7 +100,7 @@ describe('Gilded Rose', () => {
       const rose = new GildedRose([itm.item]);
 
       // Update the quality for each day within the sell in time
-      for (let days = itm.days; days > 1; days--) {
+      for (let days = itm.days; days > 0; days--) {
         rose.updateQuality();
       }
 
@@ -109,4 +109,45 @@ describe('Gilded Rose', () => {
       expect(result.quality).to.equal(itm.expected);
     });
   });
+
+  it('should update all items in the list', () => {
+    const mockData = [
+      new Item('first', 3, 10),
+      new Item('second', 0, 10),
+      new Item('third', -2, 10),
+      new Item('Aged Brie', 3, 10),
+      new Item('Conjured Thanos\' gauntlet', 3, 20),
+    ];
+    const expected = [
+      {
+        name: 'first',
+        quality: 9,
+        sellIn: 2,
+      },
+      {
+        name: 'second',
+        quality: 9,
+        sellIn: -1,
+      },
+      {
+        name: 'third',
+        quality: 8,
+        sellIn: -3,
+      },
+      {
+        name: 'Aged Brie',
+        quality: 11,
+        sellIn: 2,
+      },
+      {
+        name: 'Conjured Thanos\' gauntlet',
+        quality: 18,
+        sellIn: 2,
+      },
+    ];
+    const rose = new GildedRose(mockData);
+
+    // eql does a deep equal
+    expect(rose.updateQuality()).to.eql(expected);
+  })
 });
