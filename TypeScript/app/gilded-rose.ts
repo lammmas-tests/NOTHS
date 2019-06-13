bc8d1493f8ab9bf5c7012004bd3e3c2847eb7454
Renna <renna@reemet.me>
2019-06-13 08:36:45 +0100

    Add (failing) tests for cases outlined in doc

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -19,7 +19,7 @@ export class GildedRose {
 
     updateQuality() {
         for (let i = 0; i < this.items.length; i++) {
-            if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+            if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'AgedBrie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
                 if (this.items[i].quality > 0) {
                     if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
                         this.items[i].quality = this.items[i].quality - 1
@@ -46,7 +46,7 @@ export class GildedRose {
                 this.items[i].sellIn = this.items[i].sellIn - 1;
             }
             if (this.items[i].sellIn < 0) {
-                if (this.items[i].name != 'Aged Brie') {
+                if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'AgedBrie') {
                     if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
                         if (this.items[i].quality > 0) {
                             if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -1,12 +1,112 @@
 import { expect } from 'chai';
 import { Item, GildedRose } from '../app/gilded-rose';
 
-describe('Gilded Rose', function () {
+const items = [
+  {
+		name: 'Regular',
+		item: new Item('foo', 9, 10),
+		expected: 4,
+		days: 5
+	},
+  {
+		name: 'Last day',
+		item: new Item('foo', 9, 10),
+		expected: 2,
+		days: 9
+	},
+  {
+		name: 'Low Quality (expired)',
+		item: new Item('foo', 3, 14),
+		expected: 1,
+		days: 9
+	},
+ {
+		name: 'High Quality (expired)',
+		item: new Item('foo', 3, 94),
+		expected: 39,
+		days: 30
+	},
+  {
+		name: 'Low Quality Aged Brie',
+		item: new Item('Aged Brie', 9, 10),
+    expected: 18,
+		days: 9
+	},
+  {
+		name: 'High Quality Aged Brie',
+		item: new Item('Aged Brie', 3, 94),
+    expected: 50,
+		days: 30
+	},
+  {
+		name: 'AgedBrie',
+		item: new Item('AgedBrie', 3, 3),
+		expected: 50,
+		days: 30
+	},
+  {
+		name: 'Sulfuras, Hand of Ragnaros',
+		item: new Item('Sulfuras, Hand of Ragnaros', 9, 10),
+		expected: 80,
+		days: 9
+	},
+  {
+    name: 'Backstage pass more than 10 days away',
+    item: new Item('Backstage passes to a TAFKAL80ETC concert', 9, 14),
+    expected: 10,
+    days: 1
+  },
+  {
+    name: 'Backstage pass less than 10 days away',
+    item: new Item('Backstage passes to a TAFKAL80ETC concert', 9, 14),
+    expected: 16,
+    days: 5
+  },
+  {
+    name: 'Backstage pass less than 5 days away',
+    item: new Item('Backstage passes to a TAFKAL80ETC concert', 9, 14),
+    expected: 28,
+    days: 10
+  },
+  {
+		name: 'Backstage pass on last day',
+		item: new Item('Backstage passes to a TAFKAL80ETC concert', 9, 10),
+		expected: 30,
+		days: 9
+	},
+  {
+		name: 'Backstage pass after sell by',
+		item: new Item('Backstage passes to aTAFKAL80ETC concert', 9, 50),
+		expected: 0,
+		days: 19
+	},
+  {
+		name: 'Conjured',
+		item: new Item('Conjured Apple iPhone', 3, 14),
+		expected: 8,
+		days: 3
+	}
+ ];
 
-    it('should foo', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
+describe('Gilded Rose', () => {
+  it('should store the passed in item correctly', () => {
+    const gildedRose = new GildedRose([ new Item('somename', 0, 0) ]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).to.equal('somename');
+  });
+
+  items.forEach(itm => {
+    it(`should update quality of '${itm.name}' item correctly after ${itm.days} days has passed`, () => {
+      const rose = new GildedRose([itm.item]);
+
+      // Update the quality for each day within the sell in time
+      for (let days = itm.days; days > 1; days--) {
+        rose.updateQuality();
+      }
+
+      const result = rose.items[0];
+
+      expect(result.quality).to.equal(itm.expected);
     });
-
+  });
 });
